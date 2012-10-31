df9b7d5435fe6c9be7c8eac39bfc0cb537bfcecc
hectorprada73 <hectorprada73@hotmail.com>
2012-10-31 00:14:26 -0430

    sprite manager

--- Box2d/lib/ImageHandler.h
+++ Box2d/lib/ImageHandler.h
@@ -13,24 +13,40 @@
 
 using namespace std;
 
-class ImageHandler {
-	sf::Image image;
-	sf::Sprite sprite;
+class ImageHandler{
+
+private :
+
+    sf::Image  Image;
+    sf::Sprite Sprite;
 
 public:
 
 	//virtual ~ImageHandler();
 
 
-	sf::Sprite LoadImage(string filename)
-	{
-		if (!image.LoadFromFile(filename))
-			cout << "no cargo imagen" << endl;
+//	sf::Sprite LoadImage(string filename)
+//	{
+//		if (!image.LoadFromFile(filename))
+//			cout << "no cargo imagen" << endl;
+//
+//			sprite.SetImage(image);
+//
+//			return sprite;
+//	}
 
-			sprite.SetImage(image);
 
-			return sprite;
-	}
+	    // This is the copy constructor
+	ImageHandler(const ImageHandler& Copy) : Image(Copy.Image), Sprite(Copy.Sprite)
+	    {
+	        // This is the trick : we setup the sprite
+	        // to use our image, instead of the one of Copy
+	        Sprite.SetImage(Image);
+	    }
+
+	    // ... a lot of useful functions ...
+
+
 
 };
 
