e52d0b3b3796fec5f501cfc25a6ea5858037aaee
hectorprada73 <hectorprada73@hotmail.com>
2012-10-30 19:59:13 -0430

    tiene dos jugadores. chorizero de codigo

--- Box2d/src/main.cpp
+++ Box2d/src/main.cpp
@@ -15,24 +15,29 @@ int main() {
 	sf::Event Event;
 	//sf::Shape box = sf::Shape::Rectangle(0,0,50,50,sf::Color(127,52,100,255));
 	//sf::Sprite sprite;
-	sf::Sprite player, background;
-	sf::Image tempImage;
+	sf::Sprite player, background, agente;
+	sf::Image tempImage, agenteImage;
 	sf::Image temp2;
 
-	float velx = 0, vely = 0;
-	float x=10, y=10, moveSpeed=5;
-	int sourceX=0, sourceY=sDown;
+
+	float velx = 0, vely = 0, velx1 = 0, vely1 = 0;
+	float x=10, y=10, x1=300, y1=300, moveSpeed=8, moveSpeed1=6;
+	int sourceX=0, sourceY=sDown, sourceX1=0, sourceY1=sDown;
 
 
-	if (!temp2.LoadFromFile("/home/hector/workspace/Box2d/imagenes/map.jpg"))
+	if (!temp2.LoadFromFile("/home/hector/git/Box2d/Box2d/imagenes/map.jpg"))
 		return EXIT_FAILURE;
 		background.SetImage(temp2);
 
-	if (!tempImage.LoadFromFile("/home/hector/workspace/Box2d/imagenes/Player.png"))
+	if (!tempImage.LoadFromFile("/home/hector/git/Box2d/Box2d/imagenes/Player.png"))
 		return EXIT_FAILURE;
 		player.SetImage(tempImage);
 
-	Game.SetFramerateLimit(20);
+	if (!agenteImage.LoadFromFile("/home/hector/git/Box2d/Box2d/imagenes/Player.png"))
+		return EXIT_FAILURE;
+		agente.SetImage(agenteImage);
+
+	Game.SetFramerateLimit(27);
 	//box.SetPosition(100,100);
 
 //	sf::Image image;
@@ -131,7 +136,62 @@ int main() {
 
 			player.SetSubRect(sf::IntRect(sourceX,sourceY,sourceX + tempImage.GetWidth() /4, sourceY + tempImage.GetHeight() /4));
 			player.SetPosition(x,y);
-//			else if(Game.GetInput().IsKeyDown(sf::Key::Right))
+
+
+
+// IMPLEMENTACION PARA AGENTE
+
+			if(Game.GetInput().IsKeyDown(sf::Key::A))
+			{
+				//sprite.Move(-1,0);
+				sourceY1 = sLeft;
+				velx1 = -moveSpeed1;
+				//player.Move(-10,0);
+			}
+			else if(Game.GetInput().IsKeyDown(sf::Key::D))
+			{
+				//sprite.Move(-1,0);
+				sourceY1 = sRight;
+				velx1 = moveSpeed1;
+				//player.Move(10,0);
+
+			}
+			else
+				velx1 = 0;
+
+			if(Game.GetInput().IsKeyDown(sf::Key::W))
+			{
+				//sprite.Move(-1,0);
+				sourceY1 = sUp;
+				vely1 = -moveSpeed1;
+				//player.Move(10,-1);
+
+			}
+			else if(Game.GetInput().IsKeyDown(sf::Key::S))
+			{
+				//sprite.Move(-1,0);
+				sourceY1 = sDown;
+				vely1 = moveSpeed1;
+				//player.Move(0,10);
+
+			}
+			else
+				vely1 = 0;
+
+			x1+= velx1;
+			y1+= vely1;
+
+			if(velx1 !=0 || vely1 !=0)
+				sourceX1 += agenteImage.GetWidth() /4;
+			else
+				sourceX1= 0;
+
+			if (sourceX1 == agenteImage.GetWidth())
+				sourceX1 =0;
+
+			agente.SetSubRect(sf::IntRect(sourceX1,sourceY1,sourceX1 + agenteImage.GetWidth() /4, sourceY1 + agenteImage.GetHeight() /4));
+			agente.SetPosition(x1,y1);
+//			else if(Game.GetInput().IsKeyDºown(sf::Key::Right))
 //			{
 //				//sprite.Move(1,0);
 //
@@ -145,9 +205,10 @@ int main() {
 //			else if(Game.GetInput().IsKeyDown(sf::Key::Down))
 //							sprite.Move(0,1);
 //			Game.Draw(box);
-			Game.Clear();
-			Game.Draw(background);
+			Game.Clear(sf::Color(36,91,100,255));
+			//Game.Draw(background);
 			Game.Draw(player);
+			Game.Draw(agente);
 			//Game.Draw(sprite);
 			Game.Display();
 
